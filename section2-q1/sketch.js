508116eba9b326f66c9d6068762a233f454fd11e
yosei-yamauchi <1980617h@stu.kobe-u.ac.jp>
2020-10-20 12:22:38 +0900

    Update sketch.js

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,13 @@
 // 小手調べ
 function setup(){
+  noFill();
   for(let i = 0; i < 10; i++){
-    // BLANK[1]
+    ellipse(50, 50, 10+10*i, 10+10*i);
+    if(i<5){
+    stroke(0, 0, 225);
+    }
+    else{
+      stroke(225, 0, 0);
+    }
   }
 }
