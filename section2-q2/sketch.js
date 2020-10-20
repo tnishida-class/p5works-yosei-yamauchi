77e197b85bcfc495cd8941e30a481cd01ee6cadb
yosei-yamauchi <1980617h@stu.kobe-u.ac.jp>
2020-10-20 12:57:55 +0900

    Update sketch.js

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -5,6 +5,8 @@ function setup() {
   noStroke();
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
+      fill(196);
+      rect(10 * i, 10 * j, 5, 5);
       // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
     }
   }
