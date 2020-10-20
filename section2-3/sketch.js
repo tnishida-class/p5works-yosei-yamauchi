b9b9f19d8a2bdd25871c6d088389aef312fa0b89
yosei-yamauchi <1980617h@stu.kobe-u.ac.jp>
2020-10-20 12:44:22 +0900

    Update sketch.js

--- section2-3/sketch.js
+++ section2-3/sketch.js
@@ -5,7 +5,7 @@ function setup(){
   createCanvas(100, 100);
   for(let i = 0; i < 10; i++){
     for(let j = 0; j < 10; j++){
-      fill(j % 2 == 0 ? 255 : 0); // ここを変えます
+      fill(j % 2 == 1 ? 0 : 255); // ここを変えます
       console.log(i, j);
       rect(i * 10, j * 10, 5, 5);
     }
