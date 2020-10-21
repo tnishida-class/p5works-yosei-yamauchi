2c7a9cfeacc9ffbdd002433bb769e9d7ec3522cf
yosei-yamauchi <1980617h@stu.kobe-u.ac.jp>
2020-10-21 12:28:38 +0900

    Update sketch.js

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -6,12 +6,28 @@ function setup() {
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
       fill(196);
-      if(j%2==1){
-      rect(50 * i, 25 * j, 25, 25);
+      if(j%2==0){
+        rect(50 * i + 25, 25 * j, 25, 25);
+        if(j<3){
+        fill(255, 0, 0);
+        ellipse(37.5 + 50 * i, 12.5 + 25 * j, 20, 20);
+      }
+        else if(j>4){
+          fill(0);
+          ellipse(37.5 + 50 * i, 12.5 + 25 * j, 20, 20);
+        }
       }
       else{
-        rect(50 * i + 25, 25 * j, 25, 25);
+        rect(50 * i, 25 * j, 25, 25);
+        if(j<3){
+        fill(255, 0, 0);
+        ellipse(12.5 + 50 * i, 12.5 + 25 * j, 20, 20);
       }
+        else if(j>4){
+          fill(0);
+          ellipse(12.5 + 50 * i, 12.5 + 25 * j, 20, 20);
+        }
+     }
       // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
     }
   }
