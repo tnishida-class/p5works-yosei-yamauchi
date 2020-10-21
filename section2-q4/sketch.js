48db8448aef5b3ea7182240f75cab11565ba760d
yosei-yamauchi <1980617h@stu.kobe-u.ac.jp>
2020-10-21 22:00:48 +0900

    Update sketch.js

--- section2-q4/sketch.js
+++ section2-q4/sketch.js
@@ -2,12 +2,18 @@
 function setup() {
   const blue = color(0, 51, 160);
   createCanvas(270, 180);
-  // noStroke();
+  noStroke();// noStroke();
   background(255);
 
   let d = height / 9; // 縞1本の太さ
 
   for(let i = 0; i < 9; i++){
+    if(i%2==0){
+      fill(blue);
+    }
+    else{
+      fill(255);
+        }
     // BLANK[1] (hint: 縞の色を交互に変えるには2で割った余りを使おう)
     rect(0, i * d, width, (i + 1) * d);
   }
@@ -17,5 +23,7 @@ function setup() {
   rect(0, 0, size, size);
 
   fill(255);
+  rect(d * 2, 0, d, size);
+  rect(0, d * 2, size, d);
   // BLANK[2] (hint: 白い十字を描くには rect を二つ描こう)
 }
