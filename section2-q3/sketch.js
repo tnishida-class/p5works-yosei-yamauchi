0e9f1fe3c99ad5407218d9c560f2fd6add0227b3
yosei-yamauchi <1980617h@stu.kobe-u.ac.jp>
2020-10-27 12:27:26 +0900

    ダーツです。

--- section2-q3/sketch.js
+++ section2-q3/sketch.js
@@ -17,6 +17,10 @@ function setup() {
 
   drawCircle(black, maxR);
   drawArcs(green, red, maxR * 0.8);
+  drawArcs(cream, black, maxR * 0.75);
+  drawArcs(green, red, maxR * 0.5);
+  drawArcs(cream, black, maxR * 0.45);
+  drawCircle(green, maxR * 0.1);
   // BLANK[1] (hint: drawArcs x 3, drawCircle x 1)
   drawCircle(red, maxR * 0.05);
 }
