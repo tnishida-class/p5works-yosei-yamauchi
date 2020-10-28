74f4dcb90a36f426cc823a80ea5a836e043ef06b
yosei-yamauchi <1980617h@stu.kobe-u.ac.jp>
2020-10-28 11:40:10 +0900

    色やしっぽの位置を変えられるようにしました。

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -2,17 +2,22 @@
 // 吹き出しの位置、背景色 etc. を関数 balloon の引数で指定できるようにしてみよう
 // 吹き出しにしっぽを付けてみよう
 function setup(){
-  createCanvas(400, 400);
+  createCanvas(4000, 4000);
   background(255);
-  balloon("I love keyakizaka46");
+  let fc = color(0, 0, 255);　//文字の色　自由に変えてね
+  let bc = color(255, 0, 0);　//吹き出しの色　自由に変えてね
+  balloon("I love Nogizaka46", 20, 30, 45, 30, fc, bc);　//吹き出しの位置と　吹き出しの指す場所　自由に変えてね
 }
 
-function balloon(t){
+function balloon(t, x, y, a, b, fc, bc){
   let w = textWidth(t);
   let h = textAscent() + textDescent();
   let p = 2;
-  fill(0);
-  rect(0, 0, w + p * 2, h + p * 2);
-  fill(255);
-  text(t, p, h + p);
+  fill(bc);
+  noStroke();
+  rect(x, y, w + p * 2, h + p * 2);
+  fill(bc);
+  triangle(x , y + h + p * 2, x + 25 , y + h + p * 2, x + a, y + h + p * 2 + b);
+  fill(fc);
+  text(t, p + x, h + p + y);
 }
