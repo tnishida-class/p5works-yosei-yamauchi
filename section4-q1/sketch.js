065613aa3bb74e49d6e8694c061fcba1907270b3
yosei-yamauchi <1980617h@stu.kobe-u.ac.jp>
2020-11-17 11:51:37 +0900

    revice 4-q1
    より心臓っぽくなりました。

--- section4-q1/sketch.js
+++ section4-q1/sketch.js
@@ -1,6 +1,7 @@
 // 練習問題「心臓の鼓動のように大きくなったり小さくなったりする円」
 let count;
 let cycle;
+let size;
 
 function setup(){
   createCanvas(200, 200);
@@ -11,9 +12,21 @@ function setup(){
 function draw(){
   background(160, 192, 255);
     if(keyIsDown(" ".charCodeAt(0))){
-    count = (count + 2) % cycle;}
-    count = (count + 1) % cycle;
-  let size = count
+      count = (count + 2) % cycle;
+      if(count < 50){
+        size = 100 - count
+      }
+      else{
+        size = count
+      }
+      }
+      count = (count + 1) % cycle;
+    if(count < 50){
+      size = 100 - count
+    }
+    else{
+      size = count
+    }
   noStroke();
   fill(255, 0, 0);
   // BLANK[1]
