e9114dc4d730b5c940e91e19b94dc23960824119
yosei-yamauchi <1980617h@stu.kobe-u.ac.jp>
2020-11-18 12:19:41 +0900

    revice finalwork

--- finalwork/sketch.js
+++ finalwork/sketch.js
@@ -1,22 +1,24 @@
 // 最終課題を制作しよう
-let count1, count2, count3, count4;
 let size1, size2, size3, size4;
 let x, y;
 
 function setup(){
   createCanvas(windowWidth, windowHeight);
-  count1 = 20;
-  count2 = 10;
-  count3 = 10;
-  count4 = 0;
+  background(0, 0, 205);
+  size1 = 20;
+  size2 = 10;
+  size3 = 10;
+  size4 = 0;
+  x = random(0, windowWidth - 70);
+  y = random(70, windowHeight);
 }
 
 
 
 function draw(){
   background(0, 0, 205);
-  count1 += 1;
-  size1 = count1;
+  pochapocha();
+  size1 += 1;
   if(size1 < 100){
   noStroke();
   fill(255);
@@ -43,15 +45,13 @@ function draw(){
     ellipse(x, y, size1);
   }
 
-  count2 += 1
-  size2 = count2
+  size2 += 1
   noStroke();
   fill(0, 0, 205);
   ellipse(x, y, size2);
 
 
-  count3 += 0.5;
-  size3 = count3;
+  size3 += 0.5;
   if(size3 < 50){
   noStroke();
   fill(255);
@@ -78,18 +78,42 @@ function draw(){
     ellipse(x, y, size3);
   }
 
-  count4 += 0.5
-  size4 = count4
+  size4 += 0.5
   noStroke();
   fill(0, 0, 205);
   ellipse(x, y, size4);
+
+
+if(size1 > 200 && size2 > 200 && size3 > 100 && size4 > 100){
+  noLoop();
+}
 }
 
 function mouseClicked(){
+  size1 = 20;
+  size2 = 10;
+  size3 = 10;
+  size4 = 0;
   x = mouseX;
   y = mouseY;
+  loop();
   draw();
 }
+
+function keyPressed(){ //間違えて「スペース押したら最初の位置」という関数作ってしまってので、一応置いときます
+  if(key == " "){
+    return setup();
+    loop();
+    redraw();
+  }
+}
+
+function pochapocha(){
+  textSize(15);
+  fill(0, 206, 209);
+  text("ポチャ…", x + 10, y - 30);
+}
+
 
 function windowResized(){
   resizeCanvas(windowWidth, windowHeight);
