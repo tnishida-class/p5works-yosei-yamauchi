dcaa2ed141939df77cf3852921b6bcce3e0645bf
yosei-yamauchi <1980617h@stu.kobe-u.ac.jp>
2020-11-19 00:17:27 +0900

    finalwork revice

--- finalwork/sketch.js
+++ finalwork/sketch.js
@@ -13,7 +13,10 @@ function draw(){
   background(0, 0, 205);
 for(let i = 0; i < scores.length; i++){
   let s = scores[i];
-  pochapocha();
+  textSize(15);
+  noStroke();
+  fill(0, 206, 209);
+  text("ポチャ…", s.x + 10, s.y - 30)
   s.size1 += 1;
   if(s.size1 < 75){
   stroke(255);
@@ -75,42 +78,33 @@ for(let i = 0; i < scores.length; i++){
     fill(0, 0, 205);
     ellipse(s.x, s.y, s.size2);
   }
-
-
  }
 }
 
 function mouseClicked(){
-const s = {
+ const s = {
   x: mouseX,
   y: mouseY,
   size1: 20,
   size2: 10
-}
-
+  }
 }
 
 
 function keyPressed(){
   if(key == " "){
-for( let i = 0; i < 10; i++){
-  const s = {
-    x: random(0, windowWidth - 70),
-    y: random(70, windowHeight),
-    size1: 20,
-    size2: 10
+   for( let i = 0; i < 10; i++){
+    const s = {
+     x: random(0, windowWidth - 70),
+     y: random(70, windowHeight),
+     size1: 20,
+     size2: 10
+     }
+    scores.push(s);
     }
-  scores.push(s);
-  }
   }
 }
 
-function pochapocha(){
-  textSize(15);
-  noStroke();
-  fill(0, 206, 209);
-  text("ポチャ…", x + 10, y - 30);
-}
 
 
 function windowResized(){
