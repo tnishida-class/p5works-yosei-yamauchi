2e9923e6e5d3dc9d63a64c037d03b9f120eb6ac8
yosei-yamauchi <1980617h@stu.kobe-u.ac.jp>
2020-11-19 15:55:55 +0900

    5-1 revice

--- section5-2/sketch.js
+++ section5-2/sketch.js
@@ -14,28 +14,27 @@ function draw(){
   for(let i = 0; i < balls.length; i++){
     let b = balls[i];
     ellipse(b.x, b.y, b.size);
-    b.size += 1;
+    b.x += b.vx;
+    b.y += b.vy;
   }
 
   // 画面外に出たボールを配列から削除する（関数を引数として渡す書き方を使っています）
-  // balls = balls.filter(isInTheWindow);
+  balls = balls.filter(isInTheWindow);
 }
 
-function mouseClicked(){
-  // const dx = mouseX - pmouseX;
-  // const dy = mouseY - pmouseY;
-  // if(mag(dx, dy) > 5){
-  for(i = 0; i < 10; i++){
-    const b = { x: random(0, windowWidth), y: random(0, windowHeight), size: 20 };
+function mouseDragged(){
+  const dx = mouseX - pmouseX;
+  const dy = mouseY - pmouseY;
+  if(mag(dx, dy) > 5){
+    const b = { x: mouseX, y: mouseY, size: random(20, 100), vx: dx, vy: dy };
     balls.push(b);
-  // }
-}
+  }
 }
 
 function windowResized(){
   resizeCanvas(windowWidth, windowHeight);
 }
 
-// function isInTheWindow(b){
-//   return b.x > 0 && b.x < width && b.y > 0 && b.y < height;
-// }
+function isInTheWindow(b){
+  return b.x > 0 && b.x < width && b.y > 0 && b.y < height;
+}
